ba7f533322a3e68e50e76f346a5ccc85e0e00385
mohamedimranrmn <141555789+mohamedimranrmn@users.noreply.github.com>
2024-02-11 00:03:13 +0530

    Generate new Hadiths using API

---
+++ hadith.js
@@ -0,0 +1,39 @@
+const hadithElement = document.getElementById('hadis');
+const newHadithButton = document.getElementById('newb');
+const narratorElement = document.getElementById('narrator');
+const soundIcon = document.querySelector('.sound');
+const soundOffIcon = document.querySelector('.stop');
+
+let speech = null;
+
+async function fetchRandomHadith() {
+  try {
+    newHadithButton.innerText = 'Loading Hadith...';
+    const response = await fetch('https://random-hadith-generator.vercel.app/bukhari/');
+    const value = await response.json();
+    hadithElement.innerText = value.data.hadith_english;
+    narratorElement.innerText = value.data.header;
+    newHadithButton.innerText = 'New Hadith';
+  } catch (error) {
+    console.error('Error fetching Hadith:', error);
+    newHadithButton.innerText = 'Failed to load Hadith';
+  }
+}
+
+soundIcon.addEventListener('click', () => {
+  if (speech === null) {
+    speech = new SpeechSynthesisUtterance(hadithElement.innerText);
+    speech.onend = () => {
+      speech = null;
+    };
+    speechSynthesis.speak(speech);
+  }
+});
+
+soundOffIcon.addEventListener('click', () => {
+  if (speech !== null) {
+    speechSynthesis.cancel();
+  }
+});
+
+newHadithButton.addEventListener('click', fetchRandomHadith);
